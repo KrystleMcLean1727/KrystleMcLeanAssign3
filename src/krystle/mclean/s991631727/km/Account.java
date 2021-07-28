f6154795af2dbad50499603402808f27de654667
kmcle <kmcle@LAPTOP-N2OBMHFT>
2021-07-28 15:11:24 -0400

    Initial commit Assignment 3 Creation of Account and Student Account class + updated java doc

---
+++ src/krystle/mclean/s991631727/km/Account.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package krystle.mclean.s991631727.km;
+
+/**
+ *
+ * @author kmcle
+ * @version 1.0
+ * Description: Design Patterns implementation cohesion, generalization, etc
+ */
+//Make class immutable by making data fields private
+public abstract class Account {
+    private String email;
+    private String Password;
+    
+}
